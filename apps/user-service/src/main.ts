fb30ee72c116b295fb25d76128fa7ef36705d047
Nikita Zavada <nikitazavada463@gmail.com>
2026-06-05 08:41:40 +0300

    bbb2

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      whitelist: truee,
       forbidNonWhitelisted: true,
       transform: true,
     }),
